bf5d48b0f63c944972fd4320b244ca73e934f811
ikohunter <168725535+ikohunter@users.noreply.github.com>
2024-12-03 13:33:19 +0700

    Delete web bendicar directory

--- web bendicar/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
- $namahost = "localhost";
- $username = "root";
- $password = ""; //password MySQLi anda
- $database = "db_bendicar"; //database anda
- mysqli_connect($namahost,$username,$password) or die("Failed");
- mysqli_select_db($database) or die("Database not exist");
-?>
--- web bendicar/web/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset();  // Menghapus semua sesi
-session_destroy();  // Menghancurkan sesi
-header("Location: login.html");  // Arahkan kembali ke halaman login
-exit();
-?>
